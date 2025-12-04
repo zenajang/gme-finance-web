e5d933ad50d1d984bde799b8ddd30968aeead827
zenajang <zenaj@gmeremit.com>
2025-12-04 17:04:51 +0900

    누락된 section 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
       <CustomerFeedbackSection />
       <LoanTypesSection />
       <NewServiceSection />
-
+      <SimpleLoanApplySection />
       <FindBranchSection />
     </div>
   );
